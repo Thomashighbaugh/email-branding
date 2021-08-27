8ce012b66089501ca0252c7cf5db0b81a2b85d5b
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-08-27 15:29:55 -0700

    responsive email

---
+++ email.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script asks for the template to use, creates a folder based on input, copies the selected template to the folder and then removes the lorem ipsum, 
+## The intention is merely to streamline email creation by reducing the most tedious part and make it easily copy-pasted into a thunderbird window on the 
+## fly without needing to mess around with the HTMl/CSS/SVG
+
