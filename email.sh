468b2857a54659d92071a14a2d00a00dee1a6bd5
Thomas Highbaugh <thighbaugh@zoho.com>
2021-12-16 09:03:54 -0800

    email script update

--- email.sh
+++ email.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
+#  _______                  __ __
+# |    ___|.--------.---.-.|__|  |
+# |    ___||        |  _  ||  |  |
+# |_______||__|__|__|___._||__|__|
+###########################################################################
+# Author: Thomas Leon Highbaugh
+#
+# Description: Script asks for the template to use, creates a folder based on input, copies the selected template to the temp folder
+###########################################################################
+###########################################################################
 
-# Script asks for the template to use, creates a folder based on input, copies the selected template to the folder and then removes the lorem ipsum, 
-## The intention is merely to streamline email creation by reducing the most tedious part and make it easily copy-pasted into a thunderbird window on the 
-## fly without needing to mess around with the HTMl/CSS/SVG
+mkdir -p temp
+# TODO question asking person at company, for the headline that will be edited with sed
+# TODO Add function to retrive date and fill it into the email template
+# TODO Create coverletter template and fill it in with information retrived specifically for that situation, likely in its own function called by the switch
+echo "Who is the email to?"
 
+read WHO
+
+mkdir -p temp/"$WHO"
+
+PS3='Which Type of Email?: '
+options=("Regular" "Newsletter" "Quit")
+select opt in "${options[@]}"; do
+    case $opt in
+    "Regular")
+        cp -rvf ./templates/responsive.html temp/"$WHO".html
+        echo "Template copied to the temp folder"
+        # TODO create sed command to fill in the header
+        break
+        ;;
+    "Newsletter")
+        cp -rvf ./templates/newsletter.html temp/"$WHO"/"$WHO".html
+        echo "Template copied to the temp folder"
+        break
+        ;;
+    "Quit")
+        break
+        ;;
+    *) echo "invalid option $REPLY" ;;
+    esac
+done
